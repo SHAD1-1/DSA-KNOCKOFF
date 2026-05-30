b1fa0e13f1468dd1440d9c9e4bb28f4fa387e4eb
Fera1018 <shadwanshahabpc@gmail.com>
2026-05-30 14:10:53 +0600

    Solved a new problem

---
+++ Arrays/Problem 7.py
@@ -0,0 +1,35 @@
+arr = []
+
+# Take input
+for i in range(11):
+    num = float(input("Enter a number: "))
+    arr.append(num)
+
+print("Original Array:", arr)
+
+# Bubble Sort
+for i in range(10):
+    for j in range(10 - i):
+        if arr[j] > arr[j + 1]:
+            temp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j + 1] = temp
+
+print("Sorted Array:", arr)
+
+# Find values
+first_min = arr[0]
+second_min = arr[1]
+
+first_max = arr[10]
+second_max = arr[9]
+
+median = arr[5]
+
+print("1st Minimum:", first_min)
+print("2nd Minimum:", second_min)
+
+print("1st Maximum:", first_max)
+print("2nd Maximum:", second_max)
+
+print("Median:", median)
