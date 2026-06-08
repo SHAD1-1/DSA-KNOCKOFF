e29a952c3d0154f8e7f4ff70d73888648ce4732e
Fera1018 <shadwanshahabpc@gmail.com>
2026-06-08 13:37:15 +0600

    Solving functions now

---
+++ Functions/Problem 1.py
@@ -0,0 +1,8 @@
+#Write a function that takes a number and a character (as parameters) and prints a triangle of that size using the specified character.
+
+def triangle(size, ch):
+    for i in range(1, size + 1):
+        print(ch * i)
+
+# Example
+triangle(5, "*")
