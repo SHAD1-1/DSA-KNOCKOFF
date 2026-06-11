62cf8745cb4c8e4bd6a1a02735727d42b7df303b
Fera1018 <shadwanshahabpc@gmail.com>
2026-06-11 18:53:38 +0600

    Started Linked List

--- Functions/Triangle.py
+++ Functions/Triangle.py
@@ -1,9 +1,6 @@
-#Write a function that takes a number and a character (as parameters) and prints a triangle of that size using the specified character.
+def triangle(size,ch):
+    for i in range(size,0,-1):
+        print(ch *i)
 
-def triangle(size, ch):
-    for i in range(1, size + 1):
-        return ch * i
-    
 
-# Example
-print(triangle(5, "*"))
+triangle(5,"*")
