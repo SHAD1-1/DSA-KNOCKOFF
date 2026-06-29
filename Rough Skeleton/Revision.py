919876e170ed36fe063c607f7f4aba4f27bcae40
Fera1018 <shadwanshahabpc@gmail.com>
2026-06-30 04:57:59 +0600

    Revision work

---
+++ Rough Skeleton/Revision.py
@@ -0,0 +1,80 @@
+# LST = [1,2,3,4,5,6,7,8,9,10]
+
+def count(lst):
+    count = 0
+    for i in lst:
+        count += 1
+
+    return count
+
+usage = [1,2,3,4,5,6,7,8,9,10]
+
+print(count(usage))
+
+
+def reverse(lst):
+    result = []
+    for i in range(len(lst)-1,-1,-1):
+        result.append(lst[i])
+
+    return result
+
+print(reverse(usage))
+
+
+def AddAll(lst):
+    result = 0
+    for i in lst:
+        result += i
+
+
+    return result
+
+print(AddAll(usage))
+
+
+def highest(lst):
+    first = lst[0]
+    for i in lst:
+        if i > first:
+            first = i
+
+    return first
+
+print(highest(usage))
+
+def lowest(lst):
+    first = lst[0]
+    for i in lst:
+        if i < first:
+            first = i
+
+    return first
+
+print("Lowest:",lowest(usage))
+
+
+
+def mid(lst):
+    middle = len(lst) // 2
+    return lst[middle]
+
+print("Mid: ",mid(usage))
+
+
+def avg(lst):
+    total = 0
+
+    for i in lst:
+        total += i
+
+    return total // len(lst)
+
+print("Avg:",avg(usage))
+
+
+def second(lst):
+    
+
+
+
