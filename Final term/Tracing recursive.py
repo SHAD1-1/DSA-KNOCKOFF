e837e084628f019ee95081dab001959c4ca17a49
StarScream <shadwanshahabpc@gmail.com>
2026-07-16 00:26:30 +0600

    Didnt understand shit

--- Final term/Tracing recursive.py
+++ Final term/Tracing recursive.py
@@ -17,4 +17,15 @@ def rec(n):
     print(n, end=' ')
     rec(n + 1)
 
+
+rec(6)
+
+def rec(n):
+    if n >= 10:
+        return
+    rec(n + 2)
+    print(n, end=' ')
+    rec(n + 1)
+    
+
 rec(6)
